6e945ea6b0d7c5e48e7f8623933f799a44711a9c
jonathan <jonathanvevance@gmail.com>
2020-12-21 22:24:50 +0530

    removed lowd meek

--- utils/__init__.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Fan'
-
-
-def flatten(l):
-    r = []
-    for x in l:
-        r.extend(x)
-
-    return r
--- utils/logger.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'Fan'
-
-import logging
-
-logging.basicConfig(format='%(asctime)s (%(filename)s:%(lineno)d) %(message)s')
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
